86db2614fc14428ab9c3c0a6d8dcbddd76da91f6
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2023-09-27 14:12:59 -0500

    created requirements in idex.js

--- app/models/index.js
+++ app/models/index.js
@@ -16,8 +16,18 @@ db.sequelize = sequelize;
 
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.session = require("./session.model.js")(sequelize, Sequelize);
-db.tutorial = require("./tutorial.model.js")(sequelize, Sequelize);
-db.lesson = require("./lesson.model.js")(sequelize, Sequelize);
+db.accomCat = require("./accomCat.model.js")(sequelize, Sequelize);
+db.accommodation = require("./accommodation.model.js")(sequelize, Sequelize);
+db.course = require("./course.model.js")(sequelize, Sequelize);
+db.emailLog = require("./emailLog.model.js")(sequelize, Sequelize);
+db.facultySection = require("./facultySection.model.js")(sequelize, Sequelize);
+db.facultyStaff = require("./facultyStaff.model.js")(sequelize, Sequelize);
+db.request = require("./request.model.js")(sequelize, Sequelize);
+db.section = require("./section.model.js")(sequelize, Sequelize);
+db.semester = require("./semester.model.js")(sequelize, Sequelize);
+db.student = require("./student.model.js")(sequelize, Sequelize);
+db.studentAccom = require("./studentAccom.model.js")(sequelize, Sequelize);
+db.studentSection = require("./studentSection.model.js")(sequelize, Sequelize);
 
 // foreign key for session
 db.user.hasMany(
