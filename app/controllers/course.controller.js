ce23b8f094bd1e7776213791e6abc4c77c2a635c
theshaggy5000 <nickdrebot@gmail.com>
2023-10-12 16:57:49 -0500

    added the course controller. Begin work on course route

---
+++ app/controllers/course.controller.js
@@ -0,0 +1,116 @@
+const db = require("../models");
+const Course = db.course;
+const Op = db.Sequelize.Op;
+
+//create and save a new course
+
+exports.create = (req, res) => {
+    //validate request
+    if(!req.body.courseNumber) {
+        res.status(400).send({
+            message: "Content cannot be empty",
+        });
+        return;
+    }
+    console.log("inside course.controller.js create function");
+
+    //create a course
+
+    const course = {
+        description: req.body.description,
+    };
+    //save course in the database
+
+    Course.create(course)
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "some error occured while creating a course",
+
+            });
+        });
+};
+
+//retrieve all courses from the database
+exports.findAll = (req, res) => {
+    const id = req.query.courseNumber; //may need to change query.id to query.courseNumber
+    var condition = id ? {id: { [Op.like]: `%${id}%` } } : null;
+
+    Course.findAll({where: condition})
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: err.message || "Some error occured while retrieving the courses",
+            });
+        });
+};
+
+//Find a single course with a courseNumber
+
+exports.findOne = (req, res) => {
+    const id = req.params.courseNumber
+    ;
+
+    Course.findByPk(id)
+        .then((data) =>{
+            if(data){
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find course with id=${id}`,
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({
+                message: "error retrieving course with id=" + id,
+            });
+        });
+};
+
+//delete a course with the specified courseId in the request
+
+exports.delete = (req, res) => {
+    const id = req.params.courseNumber;
+
+    Course.destroy({
+        where: {courseNumber: id},
+    })
+    .then((num) => {
+        if(num == 1){
+            res.send({
+                message: "course was successfully deleted",
+            });
+        } else {
+            res.send({
+                message: `Cannot delete course with courseNumber=${id}. `,
+            });
+        }
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message: "Could not delete course with courseNumber = " + id,
+        });
+    });
+};
+
+//delete all courses from the database
+exports.deleteAll = (req, res) => {
+    Course.destroy({
+        where: {},
+        truncate: false,
+    })
+    .then((nums) => {
+        res.send({message: `${nums} courses were deleted successfully`});
+    })
+    .catch((err) => {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while removing all courses"
+        });
+    });
+};
