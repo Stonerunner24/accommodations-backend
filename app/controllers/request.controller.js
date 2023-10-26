7ae083415327ce18eb297ab380f248e73f7b97a7
Stonerunner24 <eli.coyle1@gmail.com>
2023-10-26 09:05:59 -0500

    Trying to fix emails in request

--- app/controllers/request.controller.js
+++ app/controllers/request.controller.js
@@ -59,7 +59,7 @@ exports.create = async (req, res) => {
 
     const mailOptions = {
         from: transporter.user,
-        to: 'd.billingsley@eagles.oc.edu',
+        to: req.body.email,
         subject: 'Test', text:
         'Four score and seven years ago our fathers brought forth, on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.'
       };
